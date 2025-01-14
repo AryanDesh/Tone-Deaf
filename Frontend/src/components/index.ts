94a0a07bd9c33e38d5812697bc96a1408c51025d
SamstyleGhost <rohancrrm@gmail.com>
2025-01-14 22:00:24 +0530

    Refactored Audio player

--- Frontend/src/components/index.ts
+++ Frontend/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Tesseract } from './Tesseract'
 export { default as Side } from './Side'
 export { default as IconButton } from './IconButton'
+export { default as AudioPlayer } from './AudioPlayer'
